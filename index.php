d8d5ce73b7d18e5b51de600e3ea7bf09792680d0
SergM2014 <weisse011@gmail.com>
2022-03-18 17:48:02 +0200

    declare(strict_types=1) +

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 require_once 'settings.php';
 
 $dto = (new \Src\Dto("php://input"))->getObject();
 $connectionService = \Src\Services\ConnectionService::getService();
 
 (new \Src\Dispatcher($routes))->run($connectionService, $dto);
-
--- settings.php
+++ settings.php
@@ -1,25 +1,25 @@
 <?php
 
-require_once 'vendor/autoload.php';
+declare(strict_types=1);
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+require_once 'vendor/autoload.php';
 
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
+use \Src\Actions\NotFound;
 
-
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 define("BOT_TOKEN", $_ENV['BOT_TOKEN']);
 define("BASIC_API_URL", 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 define("WEBHOOK_URL", $_ENV['WEBHOOK_URL']);
 define("DATA_LOGS",  __DIR__ . '/logs.txt');
-
 define("HOST", $_ENV['HOST']);
 define("USER", $_ENV['USER']);
 define("PASSWORD", $_ENV['PASSWORD']);
 define("NAME_BD", $_ENV['NAME_BD']);
-define("NOT_FOUND_ROUTE", \Src\Actions\NotFound::class);
+define("NOT_FOUND_ROUTE", NotFound::class);
 
 require_once 'routes.php';
 
