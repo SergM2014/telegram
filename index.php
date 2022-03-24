742a0f1f422a3c25a47ea9ff89a4fd24772ede00
SergM2014 <weisse011@gmail.com>
2022-03-24 12:28:58 +0200

    DTO changes

--- index.php
+++ index.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 
 require_once 'settings.php';
 
-$dto = new \Src\Dto('php://input');
+$update = file_get_contents('php://input');
+$update = json_decode($update, true);
+
+$dto = new \Src\Dto(
+    $update['message']['text'],
+    $update['message']['chat']['id'],
+    $update['message']['chat']['first_name'],
+    $update['message']['chat']['last_name']
+);
 $connectionService = new SimpleTelegramBot\Connection\CurlConnectionService();
 
 (new \Src\Dispatcher($routes))->run($connectionService, $dto);
--- src/Dto.php
+++ src/Dto.php
@@ -6,20 +6,12 @@ namespace Src;
 
 class Dto
 {
-    public string $text;
-    public int $chatId;
-    public string $firstName;
-    public string $lastName;
-
-    public function __construct($input)
-    {
-        $update = file_get_contents($input);
-        $update = json_decode($update, true);
-
-        $this->text = $update['message']['text'];
-        $this->chatId = $update['message']['chat']['id'];
-        $this->firstName = $update['message']['chat']['first_name'];
-        $this->lastName = $update['message']['chat']['last_name'];
-    }
+    public function __construct(
+         public readonly string $text,
+         public readonly int $chatId,
+         public readonly string $firstName,
+         public readonly string $lastName
+    )
+    {}
 
 }
