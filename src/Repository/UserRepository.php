eef05f2f08e0a5e7c54536af69389c91af0b2c15
SergM2014 <weisse011@gmail.com>
2022-03-24 12:06:19 +0200

    attempts to actualise exception system

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,22 +5,29 @@ declare(strict_types=1);
 namespace Src\Repository;
 
 use Src\Dto;
+use Monolog\Logger;
 use Src\Models\User;
 use Src\MyException;
+use Monolog\Handler\StreamHandler;
 use Src\Interfaces\UserRepositoryInterface;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 
 class UserRepository implements UserRepositoryInterface
 {
     public function getUserByChatId(int $id): User
     {
-        try {
-            return User::where('chat_id', $id)->first();
-            throw new MyException('myException', 'my error message');
-        } catch (MyException $e) {
-            $e->log();
-        }
-
+//        try {
+//            $user = User::where('chat_id', $id)->firstOrFail();
+//        }
+//           // throw new MyException('myException', 'my error message');
+//        catch(ModelNotFoundException $ex){
+//           // print_r($ex->getMessage());
+//            $logger = new Logger('my_logger');
+//            $logger->pushHandler(new StreamHandler(DATA_LOGS, Logger::DEBUG));
+//            $logger->info($ex->getMessage());
+//        }
+        return User::where('chat_id', $id)->firstOrFail();
     }
 
     public function createUser(Dto $dto): User
