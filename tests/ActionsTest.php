18350760a5e9714602ba5fa82dd8ef6e70de7b17
SergM2014 <weisse011@gmail.com>
2022-04-05 12:43:48 +0300

    save action test +

--- tests/ActionsTest.php
+++ tests/ActionsTest.php
@@ -3,9 +3,12 @@
 namespace Tests;
 
 use Src\Dto;
+use Monolog\Logger;
 use Src\Models\User;
+use Src\Actions\Save;
 use Src\Actions\Start;
 use DG\BypassFinals;
+use Src\Actions\ErrorOutput;
 use PHPUnit\Framework\TestCase;
 use Src\Repository\UserRepository;
 use SimpleTelegramBot\Connection\CurlConnectionService;
@@ -25,22 +28,54 @@ class ActionsTest extends TestCase
             'Serhii',
             'Me'
         );
+        //define('BASIC_API_URL', 'https://api.telegram.org/bot1edhrto;drthg;l/');
+        $this->checkBasicApiUrl();
 
+        $this->connectionService = $this->getMockBuilder(CurlConnectionService::class)
+            ->setMethods(['withArrayResponse'])
+            ->getMock();
 
-        define('BASIC_API_URL', 'https://api.telegram.org/bot1edhrto;drthg;l/');
+        $this->connectionService->expects($this->once())
+            ->method('withArrayResponse');
+
+        $this->logger = $this->createMock(Logger::class);
+        $this->errorOutput = $this->createMock(ErrorOutput::class);
+
+        $this->userRepository = $this->getMockBuilder(UserRepository::class)
+            ->onlyMethods(['createUser'])
+            ->setConstructorArgs([$this->logger, $this->errorOutput])
+            ->getMock();
+
         parent::setUp();
+    }
+    private function checkBasicApiUrl(): void
+    {
+      if(defined('BASIC_API_URL')) define('BASIC_API_URL', 'https://api.telegram.org/bot1edhrto;drthg;l/');
     }
 
     public function testStart(): void
     {
-        $connectionService = $this->getMockBuilder(CurlConnectionService::class)
-             ->setMethods(['withArrayResponse'])
-             ->getMock();
-
-        $connectionService->expects($this->once())
-            ->method('withArrayResponse');
-
-       $start = new Start($connectionService);
+       $start = new Start($this->connectionService);
        $start($this->dto);
     }
+
+    public function testSave(): void
+    {
+        $this->userRepository ->expects($this->once())
+            ->method('createUser')
+            ->with($this->dto);
+
+        $save = new Save($this->connectionService,$this->userRepository);
+        $save($this->dto);
+    }
+
+//    public function testMe():void
+//    {
+//        $this->userRepository ->expects($this->once())
+//            ->method('getUserByChatId');
+//
+//        $save = new Save($this->connectionService,$this->userRepository);
+//        $save($this->dto);
+//    }
+
 }
